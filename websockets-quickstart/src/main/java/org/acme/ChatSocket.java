d8c6d5c2416a50801603e62f6cd59dbb4200d5a2
MathiasBal <mathiiias.bal@gmail.com>
2021-11-26 12:37:16 +0100

    Implement broadcast method

--- websockets-quickstart/src/main/java/org/acme/ChatSocket.java
+++ websockets-quickstart/src/main/java/org/acme/ChatSocket.java
@@ -1,6 +1,7 @@
 package org.acme;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
@@ -16,5 +17,16 @@ public class ChatSocket {
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username) {
         sessions.put(username, session);
+    }
+
+    private void broadcast(String message) {
+        sessions.values().forEach(s -> {
+            s.getAsyncRemote().sendObject(message, result ->  {
+                if (result.getException() != null) {
+                    System.out.println("Unable to send message: " + result.getException());
+                }
+            });
+        });
+
     }
 }
